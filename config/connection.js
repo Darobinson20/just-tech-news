503ebc0afb58e5b44093db75ed7db4876b290097
Darobinson20 <Darobinson20@gmail.com>
2022-06-03 23:49:10 -0400

    fixed

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,21 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-
+console.log(
+  JSON.stringify(
+    {
+      DB_NAME: process.env.DB_NAME,
+      DB_USER: process.env.DB_USER,
+      DB_PW: process.env.DB_PASSWORD,
+    },
+    null,
+    2
+  )
+);
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
